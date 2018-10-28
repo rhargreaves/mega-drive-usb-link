05927e1e6204f511e8602e7839dc0a8f4f88e158
rhargreaves <robert@roberthargreaves.net>
2018-10-28 18:06:46 +0000

    Fix build by adding mock functions

--- tests/test_main.c
+++ tests/test_main.c
@@ -3,6 +3,10 @@
 #include <setjmp.h>
 #include <cmocka.h>
 #include <foo.h>
+#include <types.h>
+
+u16 uintToStr(u32 value, char *str, u16 minsize) {}
+void VDP_drawText(const char *str, u16 x, u16 y) {}
 
 static void adds_two_integers(void **state)
 {
